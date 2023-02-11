802c495a959dcd4f1b6bc2094be8fb715b03c692
Clinton Dean <clinton@datameshgroup.com>
2023-02-11 22:27:34 +1100

    Github actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://clintondean.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'clintondean', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-demo', // Usually your repo name.
+
+  trailingSlash: false,
+
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
